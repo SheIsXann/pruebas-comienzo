fd8db32a9695dbee8ca5f0d19f5295e46fe2d661
SheIsXann <163497133+SheIsXann@users.noreply.github.com>
2025-09-23 19:30:20 -0600

    deshabilutar boton

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ function asignarTextoElemento(elemento, texto) {
     elemetoHTML.innerHTML = texto;
     return;
 }
-function verificarIntento(){
-    alert('click desde el boton');
-    return;
-}
 asignarTextoElemento('h1','Juego del numero Secreto!');
 asignarTextoElemento('p','Indica un numero del 1 al 10');
 
@@ -21,6 +17,7 @@ function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
     if(numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p', 'Acertaste el numero');
+        document.querySelector('reiniciar').removeAttribute('disabled').value;
 
     }else{ 
         if (numeroDeUsuario > numeroSecreto){ 
